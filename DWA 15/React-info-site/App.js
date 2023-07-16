ba4c6a7e87580a7fc73804ba835fbb7a0c64f6df
kwetso12 <kgaladikwetso@gmail.com>
2023-07-16 23:08:59 +0200

    DWA

---
+++ DWA 15/React-info-site/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Navbar from "../vite-project/src/Components/Navbar"
+import Main from "../vite-project/src/Components/Main"
+
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
