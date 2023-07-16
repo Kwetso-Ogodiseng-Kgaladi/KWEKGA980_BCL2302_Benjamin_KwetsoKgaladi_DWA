4ecffe40156b0c9c37ca2dc6e374bb2145fd1874
kwetso12 <kgaladikwetso@gmail.com>
2023-07-16 23:11:54 +0200

    DWA

---
+++ DWA 15/vite-project/src/App.jsx
@@ -0,0 +1,23 @@
+// import logo from './logo.svg';
+import './App.css';
+
+// function App() {
+//   return (
+//    <>Hello World!!</>
+//   );
+// }
+
+// export default App;
+
+import React from 'react'
+import Navbar from "./Components/Navbar"
+import Main from "./Components/Main"
+
+export default function App() {
+    return (
+        <div className="container">
+            <Navbar />
+            <Main />
+        </div>
+    )
+}
